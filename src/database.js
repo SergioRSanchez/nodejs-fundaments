a94a69cf5a7d3462e7c526572dbee62efcacea6c
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-06-13 18:59:23 -0300

    feat: created update user data function

--- src/database.js
+++ src/database.js
@@ -19,11 +19,13 @@ export class Database {
     fs.writeFile(databasePath, JSON.stringify(this.#database))
   };
 
+
   select(table) {
     const data = this.#database[table] ?? [];
 
     return data;
   };
+
 
   insert(table, data) {
     if (Array.isArray(this.#database[table])) {
@@ -35,6 +37,16 @@ export class Database {
     this.#persist();
 
     return data;
+  };
+
+
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id);
+
+    if (rowIndex > -1) {
+      this.#database[table][rowIndex] = { id, ...data };
+      this.#persist();
+    }
   };
 
   delete(table, id) {

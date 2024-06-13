22398755a91e57e00d0f5123eb50d4033559f33c
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-06-13 18:11:43 -0300

    feat: explain difference between query params, route params and request body

--- src/server.js
+++ src/server.js
@@ -3,6 +3,18 @@ import http from 'node:http';
 import { json } from './middlewares/json.js';
 import { routes } from './routes.js';
 
+// Query Params: parâmetros nomeados enviados no endereço da requisição
+// utilizado para URL stateful, filtros, paginação, não obrigatórios
+// Exemplo: http://localhost:3333/users?userId=1&name=Sergio
+
+// Route Params: parâmetros não nomeados
+// utilizado para identificar recursos
+// Exemplo: http://localhost:3333/users/1
+
+// Request Body: corpo da requisição, envio de informações de um formulário
+// utilizado para criar ou alterar recursos, e passam pelo protocolo htttps que são mais seguros
+// Exemplo: http://localhost:3333/users
+
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 

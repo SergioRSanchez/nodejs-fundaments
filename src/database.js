0d7f780bf75799426a7f27d6a87efa25da7d625c
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-06-13 16:54:45 -0300

    feat: created method to persist database file

--- src/database.js
+++ src/database.js
@@ -1,6 +1,22 @@
+import fs from 'node:fs/promises';
+
+const databasePath = new URL('../db.json', import.meta.url);
+
 export class Database {
-  #database = {
-    users: [],
+  #database = {};
+
+  constructor() {
+    fs.readFile(databasePath, 'utf8')
+      .then(data => {
+        this.#database = JSON.parse(data);
+      })
+      .catch(() => {
+        this.#persist()
+      })
+  };
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database))
   };
 
   select(table) {
@@ -15,6 +31,8 @@ export class Database {
     } else {
       this.#database[table] = [data]
     };
+
+    this.#persist();
 
     return data;
   };
